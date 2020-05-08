a6abe3f275aaab5be41690d2750294176aa4b9ec
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2020-05-08 00:05:45 -0300

    Refactoring recipes.js file

--- src/app/controllers/admin/recipes.js
+++ src/app/controllers/admin/recipes.js
@@ -1,99 +1,95 @@
 const fs = require('fs')
 const data = require('../../../../data')
 
-exports.index = function(req, res) {
-    return res.render('recipes/index', { recipes: data })
-}
-
-exports.create = function(req, res) {
-    return res.render('recipes/create')
-}
-
-exports.show = function(req, res) {
-    const { id } = req.params
-
-    return res.render('recipes/show', { recipe: data[id] })
-}
-
-exports.edit = function(req, res) {
-    const { id } = req.params
-    const recipe = {
-        id,
-        ...data[id]
-    }
+module.exports = {
+    index(req, res) {
+        return res.render('recipes/index', { recipes: data })
+    },
+    create(req, res) {
+        return res.render('recipes/create')
+    },
+    show(req, res) {
+        const { id } = req.params
     
-    return res.render('recipes/edit', { recipe })
-}
-
-exports.post = function(req, res) {
-    const { image, title, author, ingredients, preparation, information } = req.body
-
-    const filteredIngredients = [ingredients].filter(function(ingredient) {
-        return ingredient != ''
-    })
-
-    const filteredSteps = [preparation].filter(function(step) {
-        return step != ''
-    })
-
-    const newRecipe = {
-        image,
-        title,
-        author,
-        ingredients: filteredIngredients,
-        preparation: filteredSteps,
-        information
+        return res.render('recipes/show', { recipe: data[id] })
+    },
+    edit(req, res) {
+        const { id } = req.params
+        const recipe = {
+            id,
+            ...data[id]
+        }
+        
+        return res.render('recipes/edit', { recipe })
+    },
+    post(req, res) {
+        const { image, title, author, ingredients, preparation, information } = req.body
+    
+        const filteredIngredients = [ingredients].filter(function(ingredient) {
+            return ingredient != ''
+        })
+    
+        const filteredSteps = [preparation].filter(function(step) {
+            return step != ''
+        })
+    
+        const newRecipe = {
+            image,
+            title,
+            author,
+            ingredients: filteredIngredients,
+            preparation: filteredSteps,
+            information
+        }
+    
+        data.push(newRecipe)
+    
+        fs.writeFile('data.json', JSON.stringify(data, null, 4), function(err) {
+            if(err)
+                return res.send('Erro na gravação do arquivo!')
+            
+            return res.redirect('/admin/recipes')
+        })
+    },
+    put(req, res) {
+        const { id, image, title, author, ingredients, preparation, information } = req.body
+    
+        const filteredIngredients = ingredients.filter(function(ingredient) {
+            return ingredient != ''
+        })
+    
+        const filteredSteps = preparation.filter(function(step) {
+            return step != ''
+        })
+    
+        const updatedRecipe = {
+            image,
+            title,
+            author,
+            ingredients: filteredIngredients,
+            preparation: filteredSteps,
+            information
+        }
+    
+        data[id] = updatedRecipe
+    
+        fs.writeFile('data.json', JSON.stringify(data, null, 4), function(err) {
+            if (err)
+                return res.send('Erro na gravação do arquivo!')
+            
+            return res.json(data[id])
+        })
+    },
+    delete(req, res) {
+        const { id } = req.body
+    
+        data.splice(id, 1)
+    
+        fs.writeFile('data.json', JSON.stringify(data, null, 4), function(err) {
+            if(err)
+                return res.send('Erro na gravação do arquivo!')
+            
+            return res.redirect('/admin/recipes')
+        })
     }
-
-    data.push(newRecipe)
-
-    fs.writeFile('data.json', JSON.stringify(data, null, 4), function(err) {
-        if(err)
-            return res.send('Erro na gravação do arquivo!')
-        
-        return res.redirect('/admin/recipes')
-    })
-}
-
-exports.put = function(req, res) {
-    const { id, image, title, author, ingredients, preparation, information } = req.body
-
-    const filteredIngredients = ingredients.filter(function(ingredient) {
-        return ingredient != ''
-    })
-
-    const filteredSteps = preparation.filter(function(step) {
-        return step != ''
-    })
-
-    const updatedRecipe = {
-        image,
-        title,
-        author,
-        ingredients: filteredIngredients,
-        preparation: filteredSteps,
-        information
-    }
-
-    data[id] = updatedRecipe
-
-    fs.writeFile('data.json', JSON.stringify(data, null, 4), function(err) {
-        if (err)
-            return res.send('Erro na gravação do arquivo!')
-        
-        return res.json(data[id])
-    })
-}
-
-exports.delete = function(req, res) {
-    const { id } = req.body
-
-    data.splice(id, 1)
-
-    fs.writeFile('data.json', JSON.stringify(data, null, 4), function(err) {
-        if(err)
-            return res.send('Erro na gravação do arquivo!')
-        
-        return res.redirect('/admin/recipes')
-    })
 }
