7e4815e1966e1a06602ab22365021404118356ea
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2020-07-19 21:56:17 -0300

    New routes

--- src/routes.js
+++ src/routes.js
@@ -44,6 +44,16 @@ routes.get('/admin/chefs/create', adminChefs.create)
 routes.get('/admin/chefs/:id', adminChefs.show)
 routes.get('/admin/chefs/:id/edit', adminChefs.edit)
 
+// Rotas de perfil de um usuário logado
+routes.get('/admin/profile', ProfileController.index) // Mostrar o formulário com dados do usuário logado
+routes.put('/admin/profile', ProfileController.put)// Editar o usuário logado
+
+// Rotas que o administrador irá acessar para gerenciar usuários
+routes.get('/admin/users', UserController.list) //Mostrar a lista de usuários cadastrados
+routes.post('/admin/users', UserController.post) //Cadastrar um usuário
+routes.put('/admin/users', UserController.put) // Editar um usuário
+routes.delete('/admin/users', UserController.delete) // Deletar um usuário
+
 routes.use((req, res) => {
     return res.render('public/not-found')
 })
