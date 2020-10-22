c55d8e4ea452ea30a5644a66eea3ea263e64d26f
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2020-10-21 23:26:40 -0300

    feat: database.sql file

---
+++ database.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE foodfy;
+
+CREATE TABLE chefs ();
+
+CREATE TABLE files ();
+
+CREATE TABLE recipe_files ();
+
+CREATE TABLE recipes ();
+
+CREATE TABLE session ();
+
+CREATE TABLE users ();
