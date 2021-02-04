090baac6b686e3d65be4edec762563f1038077ce
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2021-02-03 23:08:43 -0300

    chore: Session validator and Session Controller

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -25,16 +25,16 @@ module.exports = {
         return res.render('session/forgot-password')
     },
     async forgot(req, res) {
-        const { user } = req
-        const reset_token = crypto.randomBytes(20).toString('hex')
-        let reset_token_expires = new Date()
+        const { user } = req;
+        const reset_token = crypto.randomBytes(20).toString('hex');
+        let reset_token_expires = new Date();
 
-        reset_token_expires = reset_token_expires.setHours(reset_token_expires.getHours() + 1)
+        reset_token_expires = reset_token_expires.setHours(reset_token_expires.getHours() + 1);
 
         await User.update(user.id, {
             reset_token,
             reset_token_expires
-        })
+        });
 
         await nodemailer.sendMail({
             from: 'no-reply@foodfy.com.br',
@@ -44,24 +44,24 @@ module.exports = {
                 <h2>Esqueceu a tua senha? Nós podemos te ajudar... 😎</h2>
                 <p>Clique no link abaixo para poder cadastrar uma nova senha para a tua conta do Foodfy 👨‍🍳</p>
                 <p><a href="http://localhost:3000/reset-password?email=${user.email}&reset_token=${reset_token}" target="_blank">cadastrar nova senha</a></p>`
-        })
+        });
 
-        return res.render('session/forgot-password', { success: 'Enviamos um email pra você. Verifique a tua caixa de entrada! 😉' })
+        return res.render('session/forgot-password', { success: 'Enviamos um email pra você. Verifique a tua caixa de entrada! 😉' });
     },
     resetForm(req, res) {
         const { email, reset_token } = req.query
         return res.render('session/reset-password', { user: { email, reset_token } })
     },
     async reset(req, res) {
-        const { user } = req
-        const { password } = req.body
-        const hashedPassword = await hash(password, 8)
+        const { user } = req;
+        const { password } = req.body;
+        const hashedPassword = await hash(password, 8);
 
         await User.update(user.id, {
             password: hashedPassword,
             reset_token: '',
             reset_token_expires: ''
-        })
-        return res.render('session/login', { success: 'Senha atualizada com sucesso! 😀', user: { email: user.email } })
+        });
+        return res.render('session/login', { success: 'Senha atualizada com sucesso! 😀', user: { email: user.email } });
     }
 }
--- src/app/validators/session.js
+++ src/app/validators/session.js
@@ -48,67 +48,67 @@ module.exports = {
         return next();
     },
     async forgot(req, res, next) {
-        const { email } = req.body
+        const { email } = req.body;
         
         if(!email)
             return res.render('session/forgot-password', {
                 error: 'Informe o teu email!',
                 fields_error: { email: true }
-            })
+            });
         
-        const user = await User.find({ where: {email} })
+        const user = await User.findOne({ where: {email} });
 
         if(!user)
             return res.render('session/forgot-password', {
                 error: 'Não encontramos nenhum usuário com este email. Por favor, digite o teu email corretamente!',
                 fields_error: { email: true }
-            })
+            });
         
-        req.user = user
-        return next()
+        req.user = user;
+        return next();
     },
     async reset(req, res, next) {
-        const { reset_token, email, password, passwordConfirmation } = req.body
+        const { reset_token, email, password, passwordConfirmation } = req.body;
 
         if(!email || !password || !passwordConfirmation)
             return res.render('session/reset-password', {
                 error: 'Por favor, preencha os campos de email, senha e confirmação de senha!',
                 user: req.body,
                 fields_error: { email: true, password: true, passwordConfirmation: true }
-            })
+            });
         
         if(password !== passwordConfirmation)
             return res.render('session/reset-password', {
                 error: 'Os campos de senha e confirmação de senha devem ser iguais',
                 user: { reset_token, email, password },
                 fields_error: { passwordConfirmation }
-            })
+            });
         
-        const user = await User.find({ where: {email} })
+        const user = await User.findOne({ where: {email} });
 
         if(!user)
             return res.render('session/reset-password', {
                 error: 'Não encontramos nenhum usuário com este email. Por favor, digite o teu email corretamente!',
                 user: { reset_token, password, passwordConfirmation },
                 fields_error: { email }
-            })
+            });
         
         if(!reset_token || reset_token != user.reset_token)
             return res.render('session/forgot-password', {
                 error: 'Token inválido. Clique no link que enviamos pra o teu email ou solicite a recuperação de senha novamente!',
                 user: { email }
-            })
+            });
         
-        let now = new Date()
-        now = now.setHours(now.getHours())
+        let now = new Date();
+        now = now.setHours(now.getHours());
 
         if(now > user.reset_token_expires)
             return res.render('session/forgot-password', {
                 error: 'Token expirado. Solicite novamente a recuperação de senha!',
                 user: { email }
-            })
+            });
         
-        req.user = user
-        return next()
+        req.user = user;
+        return next();
     }
 }
